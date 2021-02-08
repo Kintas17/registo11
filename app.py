bb9f8c6f1baf50ce53f38a6661fdab3e2e66119f
Gonçalo Figueiredo <Gonalo Figueiredo@DESKTOP-A2K0GVO>
2021-02-08 16:11:42 +0000

    heroku

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 
 def herokudb():
-    Host = 'ec2-54-75-235-28.eu-west-1.compute.amazonaws.com'
-    Database = 'd2i8vmdqr6hqck'
-    User = 'dlopinbemujdnz'
-    Password = 'c2f51674e9042bcd8985d5040cfe270542eec1ed42625e1e7fde56274000d210'
+    Host = 'ec2-79-125-64-18.eu-west-1.compute.amazonaws.com'
+    Database = 'd1m09l5402mu21'
+    User = 'hxymqwuweylsvx'
+    Password = '8079173efca206fa73781c774b60e3c33897e547d7515427d23b2b586924850c'
     return psycopg2.connect(host=Host, database=Database, user=User, password=Password, sslmode='require')
 
 
@@ -16,7 +16,15 @@ def gravar(v1, v2, v3):
     ficheiro = herokudb()
     db = ficheiro.cursor()
     db.execute("CREATE TABLE IF NOT EXISTS usr (nome text,email text, passe text)")
-    db.execute("INSERT INTO usr VALUES (%s, %s, %s)", (v1, v2, code(v3)))
+    db.execute("INSERT INTO usr VALUES (%s, %s, %s)", (v1, v2, v3))
+    ficheiro.commit()
+    ficheiro.close()
+
+def gravar2(v1, v2):
+    ficheiro = herokudb()
+    db = ficheiro.cursor()
+    db.execute("CREATE TABLE IF NOT EXISTS inst (nome text, tipo text)")
+    db.execute("INSERT INTO inst VALUES (%s, %s)", (v1, v2))
     ficheiro.commit()
     ficheiro.close()
 
@@ -29,9 +37,19 @@ def existe(v1):
         valor = db.fetchone()
         ficheiro.close()
     except:
-        valor = None
+        valor=None
     return valor
 
+def existe2(v1):
+    try:
+        ficheiro = herokudb()
+        db = ficheiro.cursor()
+        db.execute("SELECT * FROM inst WHERE nome = %s", (v1,))
+        valor = db.fetchone()
+        ficheiro.close()
+    except:
+        valor=None
+    return valor
 
 def log(v1, v2):
     ficheiro = herokudb()
@@ -45,7 +63,7 @@ def log(v1, v2):
 def alterar(v1, v2):
     ficheiro = herokudb()
     db = ficheiro.cursor()
-    db.execute("UPDATE usr SET passe = %s WHERE nome = %s", (code(v2), v1))
+    db.execute("UPDATE usr SET passe = %s WHERE nome = %s", (v2, v1))
     ficheiro.commit()
     ficheiro.close()
 
@@ -57,11 +75,9 @@ def apaga(v1):
     ficheiro.commit()
     ficheiro.close()
 
-
 def code(passe):
-    import hashlib
-    return hashlib.sha3_256(passe.encode()).hexdigest()
-
+        import hashlib
+        return hashlib.sha3_256(passe.encode()).hexdigest()
 
 @app.route('/registo', methods=['GET', 'POST'])
 def route():
@@ -76,8 +92,21 @@ def route():
         elif v3 != v4:
             erro = 'A palavra passe não coincide.'
         else:
-            gravar(v1, v2, v3)
+            gravar(v1, v2, code (v3))
     return render_template('registo.html', erro=erro)
+
+
+@app.route('/instrumentos', methods=['GET', 'POST'])
+def route2():
+    erro = None
+    if request.method == 'POST':
+        v1 = request.form['inst']
+        v2 = request.form['tipo']
+        if existe2(v1):
+            erro = 'O instrumento já existe.'
+        else:
+            gravar2(v1, v2)
+    return render_template('instrumentos.html', erro=erro)
 
 
 @app.route('/')
@@ -121,17 +150,17 @@ def newpasse():
     erro = None
     if request.method == 'POST':
         v1 = request.form['utilizador']
-        v0 = request.form['apasse']
-        v2 = request.form['passe']
-        v3 = request.form['cpasse']
+        v2 = request.form['passew']
+        v3 = request.form['passe']
+        v4 = request.form['cpasse']
         if not existe(v1):
             erro = 'O Utilizador não existe.'
-        elif not log(v1, v0):
-            erro = 'A palavra passe está errada.'
-        elif v2 != v3:
+        elif v3 != v4:
             erro = 'A palavra passe não coincide.'
+        elif not log(v1, v2):
+            erro = 'A palavra passe está errada.'
         else:
-            alterar(v1, v2)
+            alterar(v1, v3)
     return render_template('newpasse.html', erro=erro)
 
 
